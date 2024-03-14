c0517f4dbb757c12c367581305c5ba624b1e62d3
GlycerolVeinz <safrankou.matvei@gmail.com>
2024-03-14 15:47:32 +0100

    started work on second function

--- hw01/hw1.rkt
+++ hw01/hw1.rkt
@@ -1,5 +1,7 @@
 #lang racket
 
+;;; Header ===========================================================================================
+
 (require racket/trace)
 (require 2htdp/image)
 
@@ -7,57 +9,58 @@
 ;;;     img->mat
 ;;;     ascii-art)
 
-
-(define chars " .,:;ox%#@")
-
-(define (intensity->charIndex intensity)
-    (floor (/ 
-                (*
-                    (length chars)
-                    (- 
-                        255 
-                        (floor intensity)
-                    )
-                )
-                256
-            ))
-)
+;;; First function part ==============================================================================
 
 (define (RGB->grayscale color)
   (+ (* 0.3 (color-red color))
      (* 0.59 (color-green color))
      (* 0.11 (color-blue color))))
 
+
+(define (discardExcess lst) empty)
+(define (wrapExcess lst) (list lst))
+
+;;; makes a matrix from a list, matrix width is given as a parameter, drops excess elements
+(define (list->mat lst size excessHandler)
+    (cond
+        [(empty? lst) empty]
+        [(< (length lst) size) (excessHandler lst)]
+        [else
+            (cons (take lst size) (list->mat (drop lst size) size))
+        ]
+    )
+)
+
 ;;; Transforms image in to matrix of grayscale values
 (define (img->mat img)
-
-    ;;; makes a matrix from a list, matrix width is given as a parameter
-    (define (list->mat lst width)
-            (cond
-                [(empty? lst) empty]
-                [(< (length lst) width) (list lst)]
-                [else
-                    (cons (take lst width) (list->mat (drop lst width) width))
-                ]
-            )
-    )
-
     (list->mat 
         (map RGB->grayscale (image->color-list img)) 
         (image-width img)
+        discardExcess
     )
 )
 
 
+;;; Second function part =============================================================================
+
+(define chars " .,:;ox%#@")
+
+(define (intensity->charIndex intensity)
+    (floor (/ (* (length chars) (- 255 (floor intensity))) 256))
+)
+
 (define (ascii-art width height chars)
-    (lambda (img-path)
-        (let* 
-            ([row 0]
-            [coll 0])
-            
+    (define (img->blockMat img width height)
+        (list->mat 
+            (list->mat 
+                (img->mat img) 
+                width 
+                discardExcess
+            ) 
+            height
+            discardExcess
         )
     )
 
-
+    
 )
-
